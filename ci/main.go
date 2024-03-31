821cadf5b3f42a7e48507e0feb1441ed6e1b0254
felipecruz91 <felipecruz91@hotmail.es>
2024-03-31 17:51:27 +0200

    Add Scout to CI workflow

--- ci/main.go
+++ ci/main.go
@@ -14,10 +14,6 @@
 
 package main
 
-import (
-	"context"
-)
-
 const (
 	defaultGoVersion = "1.22.0"
 )
@@ -27,57 +23,3 @@ var (
 )
 
 type GoDagger struct{}
-
-// func (m *GoDagger) Ci(ctx context.Context,
-// 	// source is the directory containing the Go source code
-// 	// +required
-// 	source *Directory,
-// // dockerScoutHubUser is the username for Docker Scout Hub
-// // +required
-// dockerScoutHubUser string,
-// // dockerScoutHubPassword is the password for Docker Scout Hub
-// // +required
-// dockerScoutHubPassword *Secret
-// ) error {
-
-// 	if _, err := m.Test(ctx, source, defaultGoVersion, false, true, true); err != nil {
-// 		return err
-// 	}
-
-// 	if _, err := m.BuildBinaries(ctx, source, defaultGoVersion, defaultPlatforms); err != nil {
-// 		return err
-// 	}
-
-// 	for _, platform := range defaultPlatforms {
-// 		_, err := m.DockerBuildImage(ctx, source, defaultGoVersion, platform)
-// 		if err != nil {
-// 			return err
-// 		}
-// 		// TODO: not supported to export image because it's running inside a module container?
-// 		// if _, err := ctr.Export(ctx, "image"); err != nil {
-// 		// 	return err
-// 		// }
-
-// 		// _, err = m.Cves(ctx, dockerScoutHubUser, dockerScoutHubPassword, "")
-// 		// if err != nil {
-// 		// 	return err
-// 		// }
-// 	}
-
-// 	return nil
-// }
-
-// Returns a container that echoes whatever string argument is provided
-func (m *GoDagger) ContainerEcho(stringArg string) *Container {
-	return dag.Container().From("alpine:latest").WithExec([]string{"echo", stringArg})
-}
-
-// Returns lines that match a pattern in the files of the provided Directory
-func (m *GoDagger) GrepDir(ctx context.Context, directoryArg *Directory, pattern string) (string, error) {
-	return dag.Container().
-		From("alpine:latest").
-		WithMountedDirectory("/mnt", directoryArg).
-		WithWorkdir("/mnt").
-		WithExec([]string{"grep", "-R", pattern, "."}).
-		Stdout(ctx)
-}
--- ci/scout.go
+++ ci/scout.go
@@ -1,29 +1,68 @@
 package main
 
-import "context"
+import (
+	"context"
+	"dagger/go-dagger/internal/dagger"
+	"errors"
+	"fmt"
+)
 
 const (
 	dockerScoutImage = "index.docker.io/docker/scout-cli:latest"
+
+	// vulnExitCode is the exit code returned by Docker Scout when vulnerabilities are found
+	vulnExitCode = 2
 )
 
 // DockerScoutCves displays CVEs identified in a container image
 func (m *GoDagger) DockerScoutCves(
 	ctx context.Context,
+	// source is the directory containing the image tarball
+	// +required
+	source *Directory,
 	// dockerScoutHubUser is the username for Docker Scout Hub
 	// +required
 	dockerScoutHubUser string,
 	// dockerScoutHubPassword is the password for Docker Scout Hub
 	// +required
 	dockerScoutHubPassword *Secret,
-	// imageRef is the reference to the image to analyze
+	// tarballPath is the path to the tarball containing the container image
 	// +required
-	imageRef string) (*Container, error) {
-
+	tarballPath string,
+	// onlySeverity is the severity of vulnerabilities to filter by
+	// +optional
+	// +default="critical,high"
+	onlySeverity string,
+	// exitCode returns '2' if vulnerabilities are found
+	// +optional
+	// +default=false
+	exitCode bool,
+) (*Container, error) {
 	cli := dag.Pipeline("docker-scout-cves")
 
-	return cli.Container().From(dockerScoutImage).
+	args := []string{"cves", "archive://" + tarballPath}
+	if onlySeverity != "" {
+		args = append(args, "--only-severity", onlySeverity)
+	}
+	if exitCode {
+		args = append(args, "--exit-code")
+	}
+
+	ctr, err := cli.Container().From(dockerScoutImage).
 		WithEnvVariable("DOCKER_SCOUT_HUB_USER", dockerScoutHubUser).
 		WithSecretVariable("DOCKER_SCOUT_HUB_PASSWORD", dockerScoutHubPassword).
-		WithExec([]string{"cves", imageRef}).
+		WithMountedDirectory("/tmp", source).
+		WithWorkdir("/tmp").
+		WithExec(args).
 		Sync(ctx)
+
+	var e *dagger.ExecError
+	if exitCode && errors.As(err, &e) {
+		if e.ExitCode == vulnExitCode {
+			return ctr, fmt.Errorf("failing function because --exit-code was provided and vulnerabilities were found in container image, see stdout above for details")
+		}
+		return ctr, err
+	}
+
+	return ctr, nil
 }
